d6bea3c0391213ec8833231de14b5fadea37b788
Yatharth Goswami <yatharthgoswami15@gmail.com>
2021-01-23 19:09:54 +0530

    Update A1q1

--- Assignment1/A1Q1_eight_bit_adder_top.v
+++ Assignment1/A1Q1_eight_bit_adder_top.v
@@ -9,30 +9,39 @@ module eight_bit_adder_top;
 
    eight_bit_adder ADDER (A, B, Cin, Sum, Carry);
 
+    always @ (A or B or Cin or Sum or Carry) begin
+        $display("<%d>: Input_A = %b, Input_B = %b, Input_Cin = %b, Sum = %b, Carry = %b",$time,A,B,Cin,Sum,Carry);
+    end
+
     initial begin
         A = 1; B = 1; Cin = 0;
         #1 
-        A = 4; B = 2; Cin = 1;
+        $display("\n");
+        A = 200; B = 200; Cin = 1;
         #1 
+        $display("\n");
         A = 32; B = 6; Cin = 1;
         #1
+        $display("\n");
         A = 26; B = 5; Cin = 1;
         #1
+        $display("\n");
         A = 64; B = 64; Cin = 0;
         #1
+        $display("\n");
         A = 43; B = 4; Cin = 0;
         #1
+        $display("\n");
         A = 65; B = 43; Cin = 0;
         #1
+        $display("\n");
         A = 128; B = 128; Cin = 1;
         #1
-        A = 5; B = 95; Cin = 0;
+        $display("\n");
+        A = 5; B = 220; Cin = 0;
         #1
-        A = 128; B = 95; Cin = 0;
-    end
-
-    always @ (A or B or Cin) begin
-        $monitor("<%d>: Input_A = %b, Input_B = %b, Input_Cin = %b, Sum = %b, Carry = %b",$time,A,B,Cin,Sum,Carry);
+        $display("\n");
+        A = 128; B = 96; Cin = 0;
     end
 
 endmodule
