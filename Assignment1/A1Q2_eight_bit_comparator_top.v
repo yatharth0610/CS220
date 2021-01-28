571ded8c7c6dbb941aca3ee04f3eb21fb95c27e8
Atharv Singh Patlan <56124496+AthaSSiN@users.noreply.github.com>
2021-01-28 16:29:46 +0530

    Final

--- Assignment1/A1Q2_eight_bit_comparator_top.v
+++ Assignment1/A1Q2_eight_bit_comparator_top.v
@@ -17,35 +17,37 @@ module eight_bit_comparator_top;
 
     // Initialising the inputs
     initial begin
-        Lin = 0; Ein = 1; Gin = 0; // Set Ein as 1 in inputs
-        A = 1; B = 1; 
+
+        //We vary Lin, Ein, Gin as well, which represent whether the comparison done till now yields whether a is less,equal or greater than b.
+        // This provides for extending the 8 bit comparators into more bits. This is like varying Cin in Q1
+        A = 1; B = 1; Lin = 0; Ein = 1; Gin = 0; 
         #1 
         $display("\n");
-        A = 12; B = 18; 
+        A = 12; B = 18; Lin = 0; Ein = 1; Gin = 0;
         #1 
         $display("\n");
-        A = 18; B = 15; 
+        A = 18; B = 15; Lin = 0; Ein = 1; Gin = 0;
         #1
         $display("\n");
-        A = 0; B = 0; 
+        A = 0; B = 0; Lin = 0; Ein = 1; Gin = 0;
         #1
         $display("\n");
-        A = 64; B = 0;
+        A = 64; B = 0; Lin = 0; Ein = 0; Gin = 1;
         #1
         $display("\n");
-        A = 43; B = 94;
+        A = 43; B = 94; Lin = 1; Ein = 0; Gin = 0;
         #1
         $display("\n");
-        A = 255; B = 65;
+        A = 255; B = 65; Lin = 0; Ein = 1; Gin = 0;
         #1
         $display("\n");
-        A = 255; B = 255;
+        A = 255; B = 255; Lin = 0; Ein = 1; Gin = 0;
         #1
         $display("\n");
-        A = 5; B = 95;
+        A = 5; B = 95; Lin = 0; Ein = 0; Gin = 1;
         #1
         $display("\n");
-        A = 128; B = 95;
+        A = 128; B = 95; Lin = 1; Ein = 0; Gin = 0;
     end
 
     // Displaying the final output
