fa43f9a38a9583f88854fe59a9a7f80328549773
Yatharth Goswami <yatharthgoswami15@gmail.com>
2021-03-12 20:29:30 +0530

    Correct A5Q2

--- Assignment5/A5Q2_getindex.v
+++ Assignment5/A5Q2_getindex.v
@@ -14,10 +14,10 @@ module get_index(a, b, c, d, index);
 
     // Combinational Logic for finding max_index 
     three_bit_comparator COMPARATOR1 (a, b, 1'b0, 1'b1, 1'b0, Less1, Equal1, Greater1);
-    mux_2to1 MUX1 (a, b, Less1, out1);
+    mux_2to1 MUX1 (a, b, Greater1, out1);
     three_bit_comparator COMPARATOR2 (c, d, 1'b0, 1'b1, 1'b0, Less2, Equal2, Greater2);
-    mux_2to1 MUX2 (c, d, Less2, out2);
+    mux_2to1 MUX2 (c, d, Greater2, out2);
     three_bit_comparator COMPARATOR3 (out1, out2, 1'b0, 1'b1, 1'b0, Less3, Equal3, Greater3);
-    assign index = Less3 ? {{1'b1}, Less2} : {{1'b0}, Less1};
+    assign index = Greater3 ? {{1'b1}, Greater2} : {{1'b0}, Greater1};
 
 endmodule
