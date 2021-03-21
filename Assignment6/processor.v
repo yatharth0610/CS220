a7dd08710e843c8cad9213ddfdd5778bb075b825
Atharv Singh Patlan <56124496+AthaSSiN@users.noreply.github.com>
2021-03-21 12:39:27 +0530

    A6

--- Assignment6/processor.v
+++ Assignment6/processor.v
@@ -1,4 +1,4 @@
-module processor(clk, input_valid, op, read1, read2, write, write_data, read_data_1, read_data_2, done);
+module processor(clk, input_valid, op, read1, read2, write, write_data, read_data_1, read_data_2, arith_out, done);
 
     // Inputs
     input clk;
@@ -12,6 +12,7 @@ module processor(clk, input_valid, op, read1, read2, write, write_data, read_dat
     // Outputs
     output reg [15:0] read_data_1;
     output reg [15:0] read_data_2;
+    output reg[15:0] arith_out;
     output reg done = 0;
 
     // Variables for module
@@ -23,32 +24,91 @@ module processor(clk, input_valid, op, read1, read2, write, write_data, read_dat
     wire [15:0] out1;
     wire [15:0] out2;
     wire output_valid;
+    reg [4:0] count = 0;
+    reg [15:0] math_temp;
 
     // Instantiation of reg_file module
     reg_file mem(clk, read_reg1, read_reg2, write_reg, data, valid_bits, out1, out2, output_valid);
 
     always @(negedge clk) begin
         if (output_valid == 1) begin
-            done = 1;
+            // done = 1;
             valid_bits = 3'b000;
             if (op == 3'b000) begin
+                done = 1;
             end
             else if (op == 3'b001) begin
                 read_data_1 = out1;
+                done = 1;
             end 
             else if (op == 3'b010) begin
                 read_data_1 = out1;
                 read_data_2 = out2;
+                done = 1;
             end 
             else if (op == 3'b011) begin
                 read_data_1 = out1;
+                done = 1;
             end 
             else if (op == 3'b100) begin
                 read_data_1 = out1;
                 read_data_2 = out2;
+                done = 1;
             end  
+            else if (op == 3'b101) begin
+                read_data_1 = out1;
+                read_data_2 = out2;
+                if (count < 19) begin
+                    count = count + 1;
+                end
+                else begin
+                    arith_out = math_temp;
+                    count = 0;
+                    done = 1;
+                end
+                if (count == 17) begin
+                    math_temp = read_data_1 + read_data_2;
+                end
+            end
+            else if (op == 3'b110) begin
+                read_data_1 = out1;
+                read_data_2 = out2;
+                if (count < 19) begin
+                    count = count + 1;
+                end
+                else begin
+                    arith_out = math_temp;
+                    count = 0;
+                    done = 1;
+                end
+                if (count == 17) begin
+                    math_temp = read_data_1 - read_data_2;
+                end
+            end
+            else if (op == 3'b111) begin
+                $display("<%d>, Val1 <%d>, <%d>", $time, count, done);
+                read_data_1 = out1;
+                $display("<%d>, Val1 <%d>, <%d>", $time, read_data_1);
+                if (count < 20) begin
+                    count = count + 1;
+                    $display("lol");
+                end
+                else begin
+                    arith_out = math_temp;
+                    count = 0;
+                    $display("<%d>, l", $time);
+                    done = 1;
+                end
+                if (count == 17) begin
+                    math_temp = read_data_1 << read2;
+                end
+            end
         end
     end
+
+    // if (count == 20) begin
+    //                 arith_out = 
+    //             end
 
     always @(posedge clk) begin 
         if (input_valid == 1) begin
@@ -80,6 +140,34 @@ module processor(clk, input_valid, op, read1, read2, write, write_data, read_dat
                 write_reg = write;
                 data = write_data;
             end  
+            else if (op == 3'b101) begin
+                valid_bits = 3'b111;
+                read_reg1 = read1;
+                read_reg2 = read2;
+                if(count > 17) begin
+                    write_reg = write;
+                    data = math_temp;
+                end
+            end
+            else if (op == 3'b110) begin
+                valid_bits = 3'b111;
+                read_reg1 = read1;
+                read_reg2 = read2;
+                if(count > 17) begin
+                    write_reg = write;
+                    data = math_temp;
+                end
+            end
+            else if (op == 3'b111) begin
+                $display("<%d>, Val <%d>", $time, count);
+                valid_bits = 3'b100;
+                read_reg1 = read1;
+                if(count > 17) begin
+                    valid_bits = 3'b001;
+                    write_reg = write;
+                    data = math_temp;
+                end
+            end
         end
     end
 
--- Assignment6/reg_file_top.v
+++ Assignment6/reg_file_top.v
@@ -12,14 +12,15 @@ module top;
     // Outputs
     wire signed [15:0] read_data_1;
     wire signed [15:0] read_data_2;
+    wire signed [15:0] arith_out;
     wire done;
 
     // Array to store instructions
-    reg [33:0] inst[3:0];
-    reg [3:0] counter = 0;
+    reg [33:0] inst[8:0];
+    reg [4:0] counter = 0;
 
     // Instantiation of module
-    processor P(clk, input_valid, op, read1, read2, write, write_data, read_data_1, read_data_2, done);
+    processor P(clk, input_valid, op, read1, read2, write, write_data, read_data_1, read_data_2, arith_out, done);
 
     // Setting up clk signal
     initial begin
@@ -42,6 +43,11 @@ module top;
         inst[1] = 34'b0110000100000000101111111111110111;
         inst[2] = 34'b1000000100010000110000000001000001;
         inst[3] = 34'b0100001000011000000000000000000000;
+        inst[4] = 34'b1110001100011001010000000000000000;
+        inst[5] = 34'b1010000100010001000000000000000000;
+        inst[6] = 34'b1110010001001001000000000000000000;
+        inst[7] = 34'b1100010100100001100000000000000000;
+        inst[8] = 34'b0010011000000000000000000000000000;
         #5
         op = 3'b000;
         input_valid = 1;
@@ -54,24 +60,38 @@ module top;
         end
         else if (inst[counter][33:31] == 3'b001) begin
             read1 = inst[counter][30:26];
-            $display ("<%d>: Read register : %d, Read Data: %d", $time, read1, read_data_1);
+            $display ("<%d>: Instruction: <%b>, Read register : %d, Read Data: %d", $time, inst[counter][33:31], read1, read_data_1);
         end 
         else if (inst[counter][33:31] == 3'b010) begin
             read1 = inst[counter][30:26];
             read2 = inst[counter][25:21];
-            $display ("<%d>: Read register 1: %d, Read Register 2: %d, Read Data 1: %d, Read Data 2: %d", $time, read1, read2, read_data_1, read_data_2);
+            $display ("<%d>: Instruction: <%b>, Read register 1: %d, Read Register 2: %d, Read Data 1: %d, Read Data 2: %d", $time, inst[counter][33:31], read1, read2, read_data_1, read_data_2);
         end 
         else if (inst[counter][33:31] == 3'b011) begin
             read1 = inst[counter][30:26];
-            $display ("<%d>: Read register : %d, Read Data: %d", $time, read1, read_data_1);
+            $display ("<%d>: Instruction: <%b>, Read register : %d, Read Data: %d", $time, inst[counter][33:31], read1, read_data_1);
         end 
         else if (inst[counter][33:31] == 3'b100) begin
             read1 = inst[counter][30:26];
             read2 = inst[counter][25:21];
-            $display ("<%d>: Read register 1: %d, Read Register 2: %d, Read Data 1: %d, Read Data 2: %d", $time, read1, read2, read_data_1, read_data_2);
+            $display ("<%d>: Instruction: <%b>, Read register 1: %d, Read Register 2: %d, Read Data 1: %d, Read Data 2: %d", $time, inst[counter][33:31], read1, read2, read_data_1, read_data_2);
+        end
+        else if (inst[counter][33:31] == 3'b101) begin
+            read1 = inst[counter][30:26];
+            read2 = inst[counter][25:21];
+            $display ("<%d>: Instruction: <%b>, Write Register: <%d>, Value written: <%d>", $time, inst[counter][33:31], write, arith_out);
+        end
+        else if (inst[counter][33:31] == 3'b110) begin
+            read1 = inst[counter][30:26];
+            read2 = inst[counter][25:21];
+            $display ("<%d>: Instruction: <%b>, Write Register: <%d>, Value written: <%d>", $time, inst[counter][33:31], write, arith_out);
+        end
+        else if (inst[counter][33:31] == 3'b111) begin
+            read1 = inst[counter][30:26];
+            $display ("<%d>: Instruction: <%b>, Write Register: <%d>, Value written: <%d>", $time, inst[counter][33:31], write, arith_out);
         end
         counter = counter + 1;
-        if (counter == 4) begin 
+        if (counter == 8) begin 
             $finish;
         end
         else begin
@@ -103,6 +123,27 @@ module top;
                 write = inst[counter][20:16];
                 write_data =  inst[counter][15:0];
             end  
+            else if (inst[counter][33:31] == 3'b101) begin
+                op = 3'b101;
+                read1 = inst[counter][30:26];
+                read2 = inst[counter][25:21];
+                write = inst[counter][20:16];
+                // $display ("<%d>: Instruction: <%b>, Write Register: <%d>, Value written: <%d>", $time, inst[counter][33:31], write, arith_out);
+            end
+            else if (inst[counter][33:31] == 3'b110) begin
+                op = 3'b110;
+                read1 = inst[counter][30:26];
+                read2 = inst[counter][25:21];
+                write = inst[counter][20:16];
+                // $display ("<%d>: Instruction: <%b>, Write Register: <%d>, Value written: <%d>", $time, inst[counter][33:31], write, arith_out);
+            end
+            else if (inst[counter][33:31] == 3'b111) begin
+                op = 3'b111;
+                read1 = inst[counter][30:26];
+                read2 = inst[counter][25:21];
+                write = inst[counter][20:16];
+                // $display ("<%d>: Instruction: <%b>, Write Register: <%d>, Value written: <%d>", $time, inst[counter][33:31], write, arith_out);
+            end
         end
     end
 
